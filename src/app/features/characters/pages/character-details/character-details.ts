5892954653f575cceaa7b01ca00466d68d8abafc
Henrique0498 <h.lopes.silva2015@gmail.com>
2025-10-30 14:18:49 -0300

    fix(response): ajustando o tratamento de resposta da pagina characters

--- src/app/features/characters/pages/character-details/character-details.ts
+++ src/app/features/characters/pages/character-details/character-details.ts
@@ -1,30 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CharactersApiService } from '@core/services/apis/characters';
 import { GenderFormatPipe } from '@shared/pipes/gender-format.pipe';
-import { map, switchMap } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { catchError, finalize, map, of, switchMap } from 'rxjs';
+import { NotFoundComponent } from '@shared/components/notFound/not-found';
 
 @Component({
   selector: 'character-page',
   templateUrl: './character-details.html',
   styleUrl: './character-details.scss',
-  imports: [RouterLink, CommonModule, GenderFormatPipe],
+  imports: [RouterLink, CommonModule, GenderFormatPipe, NotFoundComponent],
 })
 export class CharacterDetailsPage {
   private route = inject(ActivatedRoute);
   private apiService = inject(CharactersApiService);
+  private toast = inject(ToastrService);
+  loading = signal(false);
 
   character$ = this.route.paramMap.pipe(
     map((params) => params.get('id')),
     switchMap((id) => {
-      if (id) {
-        return this.apiService
-          .findOne(id)
-          .pipe(map((response) => ({ ...response, type: response.type || 'Desconhecido' })));
+      const numericId = Number(id);
+      if (!id || isNaN(numericId)) {
+        this.toast.error('ID inválido do personagem', 'Erro');
+        this.loading.set(false);
+        return of(null);
       }
 
-      throw new Error('ID não fornecido');
+      this.loading.set(true);
+      return this.apiService.findOne(String(numericId)).pipe(
+        catchError(() => {
+          this.toast.error('Erro ao buscar personagem', 'Erro');
+          return of(null);
+        }),
+        finalize(() => this.loading.set(false))
+      );
     })
   );
 }
