0e1cef5b1af62f95e8fa93f906dcb46e1911cddd
Henrique0498 <h.lopes.silva2015@gmail.com>
2025-10-30 11:22:50 -0300

    feat(responsive): ajustando a responsividade da tela de detalhamento de personagem

--- src/app/features/characters/pages/character-details/character-details.ts
+++ src/app/features/characters/pages/character-details/character-details.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CharactersApiService } from '@core/services/apis/characters';
+import { GenderFormatPipe } from '@shared/pipes/gender-format.pipe';
 import { map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'character-page',
   templateUrl: './character-details.html',
   styleUrl: './character-details.scss',
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, GenderFormatPipe],
 })
 export class CharacterDetailsPage {
   private route = inject(ActivatedRoute);
@@ -17,7 +18,12 @@ export class CharacterDetailsPage {
   character$ = this.route.paramMap.pipe(
     map((params) => params.get('id')),
     switchMap((id) => {
-      if (id) return this.apiService.findOne(id);
+      if (id) {
+        return this.apiService
+          .findOne(id)
+          .pipe(map((response) => ({ ...response, type: response.type || 'Desconhecido' })));
+      }
+
       throw new Error('ID não fornecido');
     })
   );
