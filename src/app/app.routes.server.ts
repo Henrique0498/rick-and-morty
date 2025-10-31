77bf8a40b03257565b4d0374be3aa4cd55024119
Henrique Lopes <h.lopes.silva2015@gmail.com>
2025-10-30 21:08:30 -0300

    fix(build): ajustando warnings da build

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,10 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'characters', renderMode: RenderMode.Prerender },
+  { path: 'episodes', renderMode: RenderMode.Prerender },
+  { path: 'locations', renderMode: RenderMode.Prerender },
+  { path: 'auth/login', renderMode: RenderMode.Prerender },
+  { path: '**', renderMode: RenderMode.Server },
 ];
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/js/dist/dropdown';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
